05a7952e2976152782e30678aada300e64d9ed38
Floyed Pinto <pinto.fl@husky.neu.edu>
2020-03-12 19:18:20 -0400

    Adding more changes

---
+++ codedeploy_scripts/applicationStart.sh
@@ -0,0 +1,14 @@
+cd /etc/profile.d
+
+env
+
+FILE=userdata.sh
+while ! test -f "$FILE"; do
+    sleep 30s
+    echo "$FILE not exist"
+done
+
+cd /home/ubuntu
+runuser -l ubuntu -c 'npm install'
+sudo npm install pm2@latest
+runuser -l ubuntu -c 'pm2 start index.js'
