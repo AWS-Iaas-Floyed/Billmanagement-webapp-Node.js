6e6472c814370ce914c5d566e2d4ccdd5aca5782
Floyed Pinto <pinto.fl@husky.neu.edu>
2020-04-09 21:39:44 -0400

    Adding ssl changes

--- API/config/database-initializer.js
+++ API/config/database-initializer.js
@@ -8,7 +8,12 @@ console.log("Current environment: " + process.env.APPLICATION_ENV);
 if (process.env.APPLICATION_ENV === 'prod') {
     seq = new Sequelize('', process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
         host: process.env.RDS_HOSTNAME,
-        dialect: 'mysql'
+        dialect: 'mysql',
+        dialectOptions: {
+            ssl: {
+              ca: fs.readFileSync('/var/mysql-certs/rds-ca-2019-root.pem')
+            }
+        }
     });
 } else {
     seq = new Sequelize('', 'root', 'password', {
